20546e21c6e73b7b5835278fc1dc01a3e1ff0867
Elise MATOS <elise.matos@hotmail.fr>
2018-01-27 21:46:00 +0100

    ajout de la méthode comparaison

--- src/main/JeuPlusMoins.java
+++ src/main/JeuPlusMoins.java
@@ -2,6 +2,21 @@ package main;
 
 public class JeuPlusMoins {
 	public static void main(String[] args) {
-		System.out.println("helloworld");
+		System.out.println(comparaison(2,1));
+
 	}
+	public static String comparaison(int i, int j) {
+		String message="";
+		if (i<j) {
+			message=i+" est plus petit que "+j;
+		}
+		else if (i>j) {
+			message=i+" est plus grand que "+j;	
+		}
+		else {
+			message="Vous avez gagné.";
+		}
+		return message;
+	}
+	
 }
