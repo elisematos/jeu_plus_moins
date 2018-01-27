e430ed84b788f528ed99f2033ef6ed3d4650c41b
Elise MATOS <elise.matos@hotmail.fr>
2018-01-27 21:55:35 +0100

    nombre aléatoire

--- src/main/JeuPlusMoins.java
+++ src/main/JeuPlusMoins.java
@@ -1,17 +1,22 @@
 package main;
 
+import java.util.Random;
+
 public class JeuPlusMoins {
 	public static void main(String[] args) {
-		System.out.println(comparaison(2,1));
-
+		
+		Random r = new Random();
+		int valeur = 1 + r.nextInt(100 - 1);
+		System.out.println(comparaison(50,valeur));
 	}
+		
 	public static String comparaison(int i, int j) {
 		String message="";
 		if (i<j) {
-			message=i+" est plus petit que "+j;
+			message="Le nombre mystérieux est plus petit.";
 		}
 		else if (i>j) {
-			message=i+" est plus grand que "+j;	
+			message="Le nombre mystérieux est plus grand.";	
 		}
 		else {
 			message="Vous avez gagné.";
