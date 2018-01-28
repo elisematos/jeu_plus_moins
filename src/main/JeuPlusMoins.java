5198b0c74f51b1c75e00f2a20f96823da4929224
Elise MATOS <elise.matos@hotmail.fr>
2018-01-28 15:56:23 +0100

    tada

--- src/main/JeuPlusMoins.java
+++ src/main/JeuPlusMoins.java
@@ -8,26 +8,38 @@ public class JeuPlusMoins {
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		System.out.println("##################################");
+		System.out.println("Bienvenue sur le jeu plus ou moins");
+		System.out.println("##################################");
 		//Génération de nombre aléatoire
-		Random r = new Random();
-		int valeur = 1 + r.nextInt(100 - 1);
+		int valeur = genererAleatoire(1,100);
+		/*Compteur*/
+		for(int i = 4; i >= 0; i--){
 		/*Comparaison entre le nombre aléatoire et le nombre choisi par le joueur*/
-		System.out.println(comparaison(entreeNombre(),valeur));
+			comparaison(entreeNombre(),valeur);
+			System.out.println("Nombre d'essais restants : "+i);
+		}
+		System.out.println("Vous avez perdu. Le nombre mystérieux était "+valeur);	
 	}
 		
-	public static String comparaison(int i, int j) {
+	public static void comparaison(int i, int j) {
 		if (i<j) {
-			return "Le nombre mystérieux est plus petit.";
+			System.out.println("Le nombre mystérieux est plus grand.");
 		}
 		else if (i>j) {
-			return "Le nombre mystérieux est plus grand.";	
+			System.out.println("Le nombre mystérieux est plus petit.");	
 		}
 		else {
-			return "Vous avez gagné.";
+			System.out.println("Vous avez gagné.");
+			System.exit(0);
 		}
 	}
 	public static int entreeNombre() {
 		System.out.println("Veuillez saisir un nombre :");
 		return sc.nextInt();
+	}
+	public static int genererAleatoire(int min,int max) {
+		Random r = new Random();
+		return min + r.nextInt(max - min);
 	}
 }
