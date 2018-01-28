4f6af6bb98746472be2968a7c2c1fa74e60344ea
Elise MATOS <elise.matos@hotmail.fr>
2018-01-28 15:10:06 +0100

    entrer un nombre

--- src/main/JeuPlusMoins.java
+++ src/main/JeuPlusMoins.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class JeuPlusMoins {
 	public static void main(String[] args) {
@@ -8,6 +9,10 @@ public class JeuPlusMoins {
 		Random r = new Random();
 		int valeur = 1 + r.nextInt(100 - 1);
 		System.out.println(comparaison(50,valeur));
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nombre :");
+		int nombre = sc.nextInt();
+		System.out.println("Vous avez saisi : " + nombre);
 	}
 		
 	public static String comparaison(int i, int j) {
